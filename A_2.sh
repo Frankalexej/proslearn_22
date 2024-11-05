1fc6d7b39cf1414f8198b0ca7164505a5329529f
FrankAlexej_ailabserver <tt115889@connect.hku.hk>
2024-11-05 23:12:07 +0800

    20241105 + 11

--- A_2.sh
+++ A_2.sh
@@ -24,7 +24,7 @@ echo "Timestamp: $ts"
 # ts="0121181130"
 
 # Loop from 1 to 10, incrementing by 1
-for (( i=1; i<=1; i++ )); do
+for (( i=2; i<=3; i++ )); do
     # Loop over each combination of arguments
     for p in "${ps[@]}"; do
         for m in "${ms[@]}"; do
